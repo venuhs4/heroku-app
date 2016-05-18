e865ffbf9379a86d685696ec88356545f13a8f3e
Venu Prasad H S <venuhs4@gmail.com>
2016-05-18 17:44:14 +0530

    first

--- heroku-app/app.js
+++ heroku-app/app.js
@@ -1,1 +1,16 @@
-﻿console.log('Hello world');
+﻿var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+        Name: 'Venu Prasad', 
+        Time: new Date(), 
+        Message: 'Hello World!'
+    }));
+});
+
+var port = Number(process.env.PORT || 3000);
+
+server.listen(port, function () { 
+    console.log(port);
+});
